39785aeaebf0933312d708e549edd6ebd7d2b0cd
engel <engeljavierpinto@gmail.com>
2020-02-16 10:43:42 -0400

    Inicié el proyecto con la configuración de tres capas. Ya tengo en INSTALLED_APP a rest_framework.

---
+++ config/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': root('api_tareas/db.sqlite3'),
+    }
+}
---
+++ config/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME':'',
+    }
+}
